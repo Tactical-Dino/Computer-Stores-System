967d71faedaacb988e608365c5e30909b107bc7a
Tactical-Dino <mahmoudkamalbsns@gmail.com>
2025-05-21 13:00:01 +0300

    init commit

--- src/com/mahmoud/computerstore/Main.java
+++ src/com/mahmoud/computerstore/Main.java
@@ -1,7 +1,17 @@
 package com.mahmoud.computerstore;
 
-public class Main {
-    public static void main (String[] args){
+import com.mahmoud.computerstore.model.CPU;
 
+/**
+ * The Main class demonstrates the creation and usage of the CPU class.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Creating a new CPU object
+        CPU myCPU = new CPU(1, "Intel", "Core i5-12600K", "LGA 1700", 125);
+
+        // Displaying the CPU details
+        System.out.println("CPU Details:");
+        System.out.println(myCPU.toString());
     }
 }
